de7cb5b009291065106834c564aa40316654bff7
linchuhao <370356141@qq.com>
2020-06-24 14:59:48 +0800

    JS homework.

---
+++ main.js
@@ -0,0 +1,13 @@
+var user = {}
+user.name = "John"
+user.surname = "Mike"
+user.name = "Peter"
+delete user.name
+console.log(user)
+var fruit = {
+apple: 20,
+pear: 20,
+peach: 10
+};
+// write your code here...
+console.log(fruit.apple + fruit.pear + fruit.peach)
